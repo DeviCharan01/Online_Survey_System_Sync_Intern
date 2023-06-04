c37e7c9ef3a4b02aa286c9f96778f94f68be76bc
DeviCharan01 <99659635+DeviCharan01@users.noreply.github.com>
2023-06-04 12:18:24 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String args[]) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+
+	public void mainPageView(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+}
